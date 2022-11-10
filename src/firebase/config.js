f8671c47f7cd7ad0834bdac4219ab6fe3f08fe90
MarianoMendelsohn <mmendelsohn@udesa.edu.ar>
2022-11-10 10:16:21 -0300

    hdbcusdbic

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXc4cDgh8nrpJpvkd_NyIIBp9jsgmV28Y",
+    authDomain: "fir-rn-95645.firebaseapp.com",
+    projectId: "fir-rn-95645",
+    storageBucket: "fir-rn-95645.appspot.com",
+    messagingSenderId: "67661644320",
+    appId: "1:67661644320:web:b1e4e291d4f532b8e0b245"
+    
+};
