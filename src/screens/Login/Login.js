53d8b9ea1e1c1278cb8c9016031a6175c295fda3
FelipeVarela22 <90226059+FelipeVarela22@users.noreply.github.com>
2022-11-23 19:51:02 -0300

    login y register terminado

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,116 +1,138 @@
-import { Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
-import React, { Component } from 'react'
-import {auth} from '../../firebase/config'
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { auth } from '../../firebase/config';
 
+//muy parecido al registro
 
 class Login extends Component {
+	constructor() {
+		super();
+		this.state = {
+			email: '',
+			pass: '',
+			error:'',
+		};
+	}
+	loginUser(email, pass) {
+		auth
+        //metodo de firebase para ingresar
+			.signInWithEmailAndPassword(email, pass)
+			.then((res) => {
+				this.setState({
+					email: '',
+					pass: '',
+				});
+				this.props.navigation.navigate('TabNavigation',{id:1,mensaje:"hola"});
+			})
+			.catch((error) => console.log(error));
+	}
 
-    constructor(props){
-        super(props)
-        this.state={
-            email:'',
-            clave:'',
-            logueado: false,
-            error: ''
-        }
-    }
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<Text style={styles.headertitle}>INGRESAR</Text>
+				</View>		
+				<View>
+					<TextInput 
+                    style={styles.field} 
+                    placeholder="email" 
+                    keyboardType="email-address" 
+                    onChangeText={(text) => this.setState({ email: text })} 
+                    value={this.state.email} 
+                    />
 
-    componentDidMount(){
-        auth.onAuthStateChanged(user => {
-            if(user !== null){
-                this.props.navigation.navigate('TabNavigation')
-            }
-        })
-    }
+					<TextInput 
+                    style={styles.field} 
+                    placeholder="password"
+                     keyboardType="default" 
+                     secureTextEntry onChangeText={(text) => this.setState({ pass: text })}
+                      value={this.state.pass} 
+                      />
 
-    loguear(email, clave){
-        auth.signInWithEmailAndPassword(email, clave)
-        .then( resp => this.props.navigation.navigate('TabNavigation'))
-        .catch( err => this.setState({error: "Todavia no tenes cuenta. Resgitrate!!!"}))
-    }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Iniciar sesión</Text>
-        <View>
-            <TextInput
-             style={styles.input}
-             onChangeText={ text => this.setState( {email: text} )}
-             placeholder='Ingresa tu email'
-             value={this.state.email}
-            />
-            <TextInput
-             style={styles.input}
-             onChangeText={ text => this.setState( {clave: text} )}
-             placeholder='Ingresa tu clave'
-             value={this.state.clave}
-             secureTextEntry={true}
-            />
-            <View>
-                <TouchableOpacity onPress={()=> this.loguear(this.state.email, this.state.clave)}>
-                    <Text style={styles.button}>Entrar</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View>
-                <Text>¿Aún no tienes una cuenta?</Text>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('Register') }>
-                    <Text style={styles.register}>Registrate</Text>
-                </TouchableOpacity>
-            </View>
-            {
-                    this.state.error !== '' ?
-                    <Text>{this.state.error}</Text>:
-                    ''
-            }
-        </View>
-      </View>
-    )
-  }
+					<TouchableOpacity style={styles.button} onPress={() => this.loginUser(this.state.email, this.state.pass)}>
+					<Text style={styles.bold}>INGRESAR</Text>
+						</TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Register')}>
+					<Text style={styles.bold}>NO TENGO CUENTA</Text>
+						</TouchableOpacity>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        paddingHorizontal:32
+	header:{
+		backgroundColor: "#FF9333",
+		width: "100%",
+		padding: 10,
+		marginBottom: 20,
+	},	
+	headertitle:{
+		color: "white",
+		textAlign: "center",
+		fontSize: 20,
+		fontWeight: "600",
+		padding: 10,
+	},
+	container:{
+		overflow: "hidden",
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+		backgroundColor: "#FFFFFF",
+		color: "#ff9f68",
+		paddingTop: 20,
+	},
+	
+	field: {
+		width: "140%",
+		backgroundColor: "#E5E5E5",
+		textAlign: "center",
+		padding: 7,
+		marginTop: 5,
+		borderRadius: 15,
+	  },
+	  title: {
+		color: "#000000",
+		textAlign: "center",
+		fontSize: 20,
+		fontWeight: "600",
+		padding: 10,
+	  },
+	  bold:{
+		fontWeight: "bold",
+			button: {
+        padding:8,
+        backgroundColor:'#552586',
+        borderRadius:8,
+        textAlign:'center',
+        marginVertical:8,
+        marginHorizontal:16,
+        width:280
     },
-    
-    title:{
-        textAlign: 'center',
-        fontSize: 25,
-        marginBottom: 15,
-        fontWeight: 'bold',
-        color: '#0095F6',
+	campo: {
+        fontSize:16,
+        borderColor: '#552586',
+        borderWidth:1,
+        borderStyle:'solid',
+        borderRadius:4,
+        marginVertical:8,
+        marginHorizontal:16,
+        padding:8,
+        width:280
     },
-    
-    input:{
-        borderColor: '#ccc',
-        borderWidth: 2,
-        marginBottom: 10,
-        padding: 10,
-        fontSize: 15,
-        borderRadius: 5,
+	button: {
+        padding:8,
+        backgroundColor:'#FF9333',
+        borderRadius:8,
+        textAlign:'center',
+        marginVertical:8,
+        marginHorizontal:16,
+        width:280
     },
+	}
+});
 
-    button:{
-        textAlign: 'center',
-        backgroundColor: '#0095F6',
-        padding: 5,
-        borderRadius: 8,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        marginBottom: 5,
-        fontWeight: 'bold',
-        color:'#FFFFFF',
-        fontSize: 17,
-    },
-
-    register:{
-        color: '#0095F6',
-        fontWeight: 'bold'
-    }
-})
-
-export default Login
+export default Login;
--- src/screens/Register/Register.js
+++ src/screens/Register/Register.js
@@ -1,164 +1,258 @@
-import { Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
-import React, { Component } from 'react'
-import * as ImagePicker from 'expo-image-picker';
-import { db,  auth } from '../../firebase/config'
-import {storage} from '../../firebase/config'
+import { auth, db } from '../../firebase/config';
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+ import * as ImagePicker from 'expo-image-picker'; 
 
 
 class Register extends Component {
 
-    constructor(){
-        super()
-        this.state={
-            email:'',
-            clave:'',
-            usuario:'',
-            biografia:'',
-            foto:'',
-            error:''
-        }
-    }
+	//creamos props con estados vacios
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			pass: '',
+			nombreUsuario: '',
+			bio: '',
+			error: {
+				email: '',
+				nombreUsuario: '',
+				pass: '',
+				bio: '',
+			},
+			image: '',
+            permission: false
+		};
+	}
+	componentDidMount() {
+		 auth.onAuthStateChanged((user) => {
+			if (user) {
+				this.props.navigation.navigate('HomeMenu');
+			}
+		}); 
+	}
+	//Al registrar un user, queremos guardarlo en la db con nombre,biografia.
 
-    registrar(email, clave){
-        if (this.state.usuario == '') {
-            this.setState({error:"El usuario no puede quedar vacío"})
-          }
-        else {
-        auth.createUserWithEmailAndPassword(email, clave)
-        .then(resp => {
-            db.collection('users').add({
-                email: auth.currentUser.email,
-                usuario: this.state.usuario,
-                createdAt: Date.now(), 
-                clave: this.state.clave,
-                biografia: this.state.biografia,
-                foto: this.state.foto
-            })
-        })
-        .then( resp => this.props.navigation.navigate('Login'))
-        .catch( err => this.setState({error:err.message}))
-    }}
-  
-      pickImage(){
-        ImagePicker.launchImageLibraryAsync() // usuario elige entre sus fotos
-        .then(resp => {
-            fetch(resp.uri) 
-            .then(data => data.blob()) // Paso la uri a BLOB = Binary Large OBject
-            .then(image => {
-                const ref = storage.ref(`fotosDePerfil/${Date.now()}.jpg`) // Aclaro donde y como se guarda lo foto en el storage de firebase
-                ref.put(image) // Guardo la imagen en esa ubicación
-                .then(()=> {
-                    ref.getDownloadURL() // Recibo la url de la foto para guardarla en la base de datos
-                    .then(url => {
-                            this.setState({foto:url}) // Guardo la url en el estado
-                        }
-                    )
+	registerUser(email, pass, nombreUsuario, bio, ) {
+		//Chequear si estan vacios los campos
+		//Si estan vacios, seteame el estado error a un mesaje
+		//Despues pones return
+		if (this.state.email === '' || this.state.nombreUsuario === '' || this.state.pass === '') {
+			this.setState({ error: 'Todos los campos son obligatorios' })
+			return
+		}
+		
+	 	fetch(this.state.image)
+        .then(res=>res.blob())
+        .then(image=>{
+            const ref = storage.ref(`perfil/${Date.now()}.jpg`)
+            ref.put(image)
+            .then(()=>{
+                ref.getDownloadURL()
+                .then(()=>{
+                    this.onImageUpload(image)
+
                 })
             })
-            .catch(err => console.log(err))
         })
-        .catch(err => console.log(err))
-    };
+        .catch(err=>console.log(err))
+		auth
+			//metodo de firebase para crear usuario
+			.createUserWithEmailAndPassword(email, pass)
+			.then((res) => {
+				db
+					//creamos usuario en la base de datos
+					.collection('users')
+					.add({
+						email: this.state.email,
+						nombreUsuario: this.state.nombreUsuario,
+						bio: this.state.bio,
+						image: this.state.image
 
-    render() {
-        return (
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.title}>Completa el formulario</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder='Escribi tu email'
-                    onChangeText={text => this.setState({email: text})}
-                    value={this.state.email}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder='Escribi tu contraseña'
-                    onChangeText={text => this.setState({clave: text})}
-                    value={this.state.clave}
-                    secureTextEntry={true}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder='Nombre de usuario'
-                    onChangeText={text => this.setState({usuario: text})}
-                    value={this.state.usuario}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder='Biografía'
-                    onChangeText={text => this.setState({biografia: text})}
-                    value={this.state.biografia}
-                />
-                <View>
-                    <TouchableOpacity onPress={()=> this.pickImage()}>
-                        <Text style={styles.botton}>Foto de perfil</Text>
-                    </TouchableOpacity>
-                </View>
-                <View>
-                    <TouchableOpacity onPress={()=> this.registrar(this.state.email, this.state.clave)}>
-                        <Text style={styles.botton}>Registrarme</Text>
-                    </TouchableOpacity>
-                </View>
+					})
+					res.user.updateProfile({
+						displayName: nombreUsuario,
+					})
+					//reiniciamos el state a 0
+					.then((res) => {
+						this.setState({
+							email: '',
+							pass: '',
+							bio: '',
+							image: ''
 
-                <View>
-                    <Text>¿Ya tienes una cuenta?</Text>
-                    <TouchableOpacity onPress={()=> this.props.navigation.navigate('Login')}>
-                        <Text style={styles.loguin}>Iniciar sesión</Text>
-                    </TouchableOpacity>
-                </View>
-                {
-                    this.state.error !== '' ?
-                    <Text>{this.state.error}</Text>:
-                    ''
-                }
-            </View>
-        </View>
-        )
+						});
+						//una vez creado el usuario que te lleve al menu
+						/* this.props.navigation.navigate('HomeMenu'); */
+					});
+			})
+			.catch((error) => console.log(error));
+	}
+ 	onImageUpload(image){
+        this.setState({image: image}, () => {console.log(this.state.image)}
+        ) 
     }
+
+    elegirImagen(){
+        ImagePicker.getMediaLibraryPermissionsAsync() // no funciona el permiso
+        .then(()=>this.setState({
+            permission: true
+        }))
+        .catch(err=>console.log(err))
+        
+        let image = ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        })
+        .then((res) => {
+            if (!image.cancelled) {
+                this.setState({image: res.uri})
+                
+            }
+        })
+    } 
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<Text style={styles.headertitle}>REGISTRO</Text>
+					<Text style={styles.errorText}>
+						{this.state.error.email && 'La dirección de email es obligatoria'}
+					</Text>
+				</View>
+
+				<TextInput
+
+					style={styles.field}
+					placeholder="email"
+					keyboardType="email-address"
+					onChangeText={(text) => this.setState({ email: text })}
+					value={this.state.email} />
+				<Text style={styles.errorText}>
+					{this.state.error.nombreUsuario && 'El nombre de usuario es obligatorio'}
+				</Text>
+				<TextInput
+					style={styles.field}
+					placeholder="Nombre de usuario"
+					keyboardType="default"
+					onChangeText={(text) => this.setState({ nombreUsuario: text })}
+					value={this.state.nombreUsuario}
+				/>
+				<Text style={styles.errorText}>
+					{this.state.error.bio && 'La biografía es obligatoria'}
+				</Text>
+				<TextInput
+					style={styles.field}
+					placeholder="Biografía"
+					keyboardType="default"
+					onChangeText={(text) => this.setState({ bio: text })}
+					value={this.state.bio}
+				/>
+				<Text style={styles.errorText}>
+					{this.state.error.pass && 'La contraseña es obligatoria'}
+				</Text>
+				<TextInput
+					style={styles.field}
+					placeholder="password"
+					keyboardType="default"
+					secureTextEntry onChangeText={(text) => this.setState({ pass: text })}
+					value={this.state.pass}
+				/>
+				   <TouchableOpacity 
+                        style={styles.campo}
+                        onPress={()=>{this.elegirImagen()}}
+                    >
+                        <Text style={styles.perfil}>Elegí tu foto de perfil</Text>
+                    {/* {this.state.image && <Image source={{uri: this.state.image}} style={{width: 200, height: 200}}/>} */}
+                    </TouchableOpacity>
+
+
+
+
+
+				{/* si toco tengo cuenta que me lleve al login */}
+				<TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Login')}>
+					<Text style={styles.bold}>YA TENGO CUENTA</Text>
+					</TouchableOpacity>
+
+				{/*  cuando tocamos el boton registrarme con el metodo Onpress
+                     con un callback llamamos a la funcion registerUser y creamos el usuario */}
+				<TouchableOpacity style={styles.button} onPress={() => this.registerUser(this.state.email, this.state.pass, this.state.nombreUsuario, this.state.bio, this.state.image)}>
+					<Text style={styles.bold}>REGISTRARME</Text>
+				</TouchableOpacity>
+			</View>
+
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        paddingHorizontal:32
+	header: {
+		backgroundColor: "#FF9333",
+		width: "100%",
+		padding: 10,
+		marginBottom: 20,
+	},
+	headertitle: {
+		color: "white",
+		textAlign: "center",
+		fontSize: 20,
+		fontWeight: "600",
+		padding: 10,
+	},
+	container: {
+		overflow: "hidden",
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+		backgroundColor: "#FFFFFF",
+		color: "#ff9f68",
+		paddingTop: 20,
+	},
+	/* form:{
+		backgroundColor: 'red',
+	}, */
+	field: {
+		width: "50%",
+		backgroundColor: "#E5E5E5",
+		textAlign: "center",
+		padding: 7,
+		marginTop: 5,
+		borderRadius: 15,
+	},
+	title: {
+		color: "#000000",
+		textAlign: "center",
+		fontSize: 20,
+		fontWeight: "600",
+		padding: 10,
+	},
+	bold:{
+		fontWeight: "bold",
+	},
+	button: {
+        padding:8,
+        backgroundColor:'#FF9333',
+        borderRadius:8,
+        textAlign:'center',
+        marginVertical:8,
+        marginHorizontal:16,
+        width:280
     },
-
-    input:{
-        borderColor: '#ccc',
-        borderWidth: 2,
-        marginBottom: 10,
-        padding: 10,
-        fontSize: 15,
-        borderRadius: 5,
+	button2: {
+        padding:8,
+        backgroundColor:'grey',
+        borderRadius:8,
+        textAlign:'center',
+        marginVertical:8,
+        marginHorizontal:16,
+        width:280
     },
+		
+});
 
-    title:{
-        textAlign: 'center',
-        fontSize: 24,
-        marginBottom: 15,
-        fontWeight: 'bold',
-        color: '#0095F6',
-    },
 
-    botton:{
-        textAlign: 'center',
-        backgroundColor: '#0095F6',
-        padding: 5,
-        borderRadius: 8,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        marginBottom: 5,
-        fontWeight: 'bold',
-        color:'#FFFFFF',
-        fontSize: 17,
-    },
-
-    loguin:{
-        color: '#0095F6',
-        fontWeight: 'bold'
-    }
-})
-
-export default Register
+export default Register;
